646f9a7ffbc2206ed3b6d7819ac1c549a1c78a79
Caren-Koroeny <carensiya5@gmail.com>
2023-05-10 16:14:53 +0300

    Calc users age in 10,20,30 and 40 years

--- age.rb
+++ age.rb
@@ -5,3 +5,9 @@ print "How old are you? "
 # Get the user's input and convert it to an integer
 age = gets.chomp.to_i
 
+# Calculate the user's age in 10, 20, 30, and 40 years
+age_in_10_years = age + 10
+age_in_20_years = age + 20
+age_in_30_years = age + 30
+age_in_40_years = age + 40
+
