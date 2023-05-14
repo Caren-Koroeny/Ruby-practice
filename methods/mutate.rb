c4dee5f2a835ba373a730eb412dfcab73e4425a8
Caren-Koroeny <carensiya5@gmail.com>
2023-05-14 10:52:56 +0300

    No mutate

--- methods/mutate.rb
+++ methods/mutate.rb
@@ -18,3 +18,23 @@ end
 p "Before mutate method: #{a}"
 mutate(a)
 p "After mutate method: #{a}"
+ 
+#Here's what's happening:
+
+#We print out a as we initially defined it.
+#We print out the value returned by the mutate method.
+#We print out the value of a after the mutate method.
+
+#Let's contrast this with a method that does not mutate the argument but still returns the same value.
+
+
+a = [1, 2, 3]
+
+# Example of a method definition that does not mutate the argument
+def no_mutate(array)
+  array.last
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
