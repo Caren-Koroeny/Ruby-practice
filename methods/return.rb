fdee979d8966234892ee020bbb8984cab8fc149d
Caren-Koroeny <carensiya5@gmail.com>
2023-05-14 10:57:34 +0300

    used return keyword

---
+++ methods/return.rb
@@ -0,0 +1,19 @@
+def add_three(number)
+    number + 3
+  end
+  
+  returned_value = add_three(4)
+  puts returned_value
+
+#we're assigning the returned_value to the value returned by the add_three method. 
+#Then we print returned_value to the output to see what it has inside it. 
+#Your output should print 7 because that's what the method call returned.
+
+
+#If you wanted to explicitly return a value you can use the return keyword.
+def add_three(number)
+    return number + 3
+  end
+  
+  returned_value = add_three(4)
+  puts returned_value
